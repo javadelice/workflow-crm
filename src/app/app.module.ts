4ddc25f616316dce165870ebb6f494530d43ca91
Clement BRIEUC <clementbrieuc@gmail.com>
2019-07-24 11:44:06 +0200

    c2 component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { C2Component } from './c2/c2.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    C2Component
   ],
   imports: [
     BrowserModule,
